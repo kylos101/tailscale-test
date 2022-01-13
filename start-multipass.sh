48c3d74f0d85d20548054ce5ba87ed495314ebb3
Kyle Brennan <kylos101@gmail.com>
2022-01-12 20:20:15 -0500

    Be more explicit with port/testing

--- start-multipass.sh
+++ start-multipass.sh
@@ -6,7 +6,7 @@ yq eval '.users[].ssh-authorized-keys = strenv(PUBKEY)' - > my-config.yaml
 
 multipass launch -n database --cloud-init my-config.yaml
 echo "Start tailscale: 'sudo tailscale up' once you're in the VM."
-echo "Check on the IP binding: 'sudo ss -tap | grep postgresql' once you're in the VM."
+echo "Check on the IP binding: 'sudo ss -tapn | grep 5432' once you're in the VM."
 echo "Check the tailscale: 'tailscale status'."
 echo "Check postgresql port binding: 'ping your-gitpod-workspace'."
 echo "Check postgresql port binding: 'netcat localhost 5432 -v'."
